108bafce486378dc66410b40ab6a7f143ea3a726
Marwa1902 <bloushiya007@gmail.com>
2024-09-04 16:52:35 +0400

    update
    worked on the css file only
    edited the view of  chat section with user info, icons

--- chatapp/src/components/lists/chatList/chatList.jsx
+++ chatapp/src/components/lists/chatList/chatList.jsx
@@ -1,5 +1,8 @@
+import { useState } from "react"
 import "./chatList.css"
+
 const ChatList = () => {
+    const [addMode, setAddMode] = useState(false); //when we click the plus/add sign, the image will change to minus
     return (
         <div className='chatList'>
             <div className="search">
@@ -7,7 +10,37 @@ const ChatList = () => {
                     <img src="./search.png" alt=""/>
                     <input type="text" placeholder="Search" />
                 </div>
-                <img src="./plus.png" alt=""/>
+                <img src={addMode ? "./minus.png" : "./plus.png"} alt="" className="add"
+                onClick={() => setAddMode((prev) => !prev)} //condition for line 5, if false = minus img, else plus
+                /> 
+            </div>
+            <div className="item">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <span> Marwa </span>
+                    <p> السلام عليكم </p>
+                </div>
+            </div>
+            <div className="item">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <span> Marwa </span>
+                    <p> السلام عليكم </p>
+                </div>
+            </div>
+            <div className="item">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <span> Marwa </span>
+                    <p> السلام عليكم </p>
+                </div>
+            </div>
+            <div className="item">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <span> Marwa </span>
+                    <p> السلام عليكم </p>
+                </div>
             </div>
         </div>
     )
